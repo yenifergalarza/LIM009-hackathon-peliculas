9b2647480f91b92a835587421ae8cb803b8ae9a0
PilarFernandez1304 <ccentepilar@gmail.com>
2019-04-16 12:47:24 -0500

    dando formatos

--- js/movie.js
+++ js/movie.js
@@ -1,34 +1,34 @@
-const btnSearch=document.getElementById('btnSearch');
-const conteiner=document.getElementById('rootCard');
-btnSearch.addEventListener('click', ()=>{
-    conteiner.innerHTML='';
-    const search=document.getElementById('searchText').value;
-    const SearchMovie=encodeURIComponent(search);
-    window.getConnection(SearchMovie);
-    limpiar();
+const btnSearch = document.getElementById('btnSearch');
+const conteiner = document.getElementById('rootCard');
+btnSearch.addEventListener('click', () => {
+  conteiner.innerHTML = '';
+  const search = document.getElementById('searchText').value;
+  const SearchMovie = encodeURIComponent(search);
+  window.getConnection(SearchMovie);
+  limpiar();
 });
-const drawMovie= element=>{
-  const html=`
+const drawMovie = element => {
+  const html = `
   <div class="conteiner-movie">
       <h3>${element.Title}</h3>
       <img src="${element.Poster}"/>
   </div>
   `;
-  conteiner.insertAdjacentHTML('beforeEnd',html);
+  conteiner.insertAdjacentHTML('beforeEnd', html);
 }
 
-const limpiar=()=>{
-document.getElementById('Search').reset();
+const limpiar = () => {
+  document.getElementById('Search').reset();
 };
 const printCinema = document.getElementById("rootCard");
-   fetch("https://www.omdbapi.com/?apikey=ea8492c7&s=batman") 	
-   .then(resp =>{return resp.json();})	
-    .then(data=> console.log(data.Search))	
- 
-   const eachMovieSeries = (movie) => {
-    for (let cinemaCount = 0; cinemaCount < movie.length; cinemaCount++) {
-      if (movie[cinemaCount].hasOwnProperty('Title')) {
-        printCinema.innerHTML += `
+fetch("https://www.omdbapi.com/?apikey=ea8492c7&s=batman")
+  .then(resp => { return resp.json(); })
+  .then(data => console.log(data.Search))
+
+const eachMovieSeries = (movie) => {
+  for (let cinemaCount = 0; cinemaCount < movie.length; cinemaCount++) {
+    if (movie[cinemaCount].hasOwnProperty('Title')) {
+      printCinema.innerHTML += `
          <div class="card">
        <img src="${movie[cinemaCount].Poster}" class="card-img-top" > 
        <div class="card-body card-header">
@@ -40,8 +40,8 @@ const printCinema = document.getElementById("rootCard");
      
        </ul>
        </div> `
-      } else {
-        printCinema.innerHTML += `
+    } else {
+      printCinema.innerHTML += `
          <div class="card" ">
        <img src="${movie[cinemaCount].Poster}" class="card-img-top" > 
        <div class="card-body card-header">
@@ -53,39 +53,39 @@ const printCinema = document.getElementById("rootCard");
       
        </ul>
        </div> `
-      }
     }
   }
-   const getDataUpPoke = (data)=>{
-   function upCinema() {
-     printCinema.innerHTML = ``;
-     eachMovieSeries(data.sort(sortArrsToObjects));
-      }
-   return upCinema();
- }
-  const getDataDownPoke = (data)=>{
-     function downCinema() {
-       printCinema.innerHTML = ``;
-       eachMovieSeries(data.sort(sortArrsToObjects).reverse());
-        }
-     return downCinema();
-   }
- const searchTitle = document.getElementById("searchTitle");
-  const AzButton =document.getElementById("AzButton")
-  const title = "game of thrones"
- function traer (title){
-   fetch(`https://www.omdbapi.com/?s="${title}"&apikey=ea8492c7 `) 
-   .then(resp =>{return resp.json()})
-   .then(data=> getDataUpPoke(data.Search))
-    .catch(error=>console.log("Error",error))	   .catch(error=>console.log("Error",error))
- };
- traer(); 	AzButton.addEventListener("click",()=>{traer(title)});
-  function traer1 (title){
-     fetch(`https://www.omdbapi.com/?s="${title}"&apikey=ea8492c7`) 
-     .then(resp =>{return resp.json()})
-     .then(data=> getDataDownPoke(data.Search))
-      .catch(error=>console.log("Error",error))
-   };
- const ZaButton = document.getElementById("ZaButton");
- ZaButton.addEventListener("click",
- ()=>{traer1(title)}); 
+}
+const getDataUpPoke = (data) => {
+  function upCinema() {
+    printCinema.innerHTML = ``;
+    eachMovieSeries(data.sort(sortArrsToObjects));
+  }
+  return upCinema();
+}
+const getDataDownPoke = (data) => {
+  function downCinema() {
+    printCinema.innerHTML = ``;
+    eachMovieSeries(data.sort(sortArrsToObjects).reverse());
+  }
+  return downCinema();
+}
+const searchTitle = document.getElementById("searchTitle");
+const AzButton = document.getElementById("AzButton")
+const title = "game of thrones"
+function traer(title) {
+  fetch(`https://www.omdbapi.com/?s="${title}"&apikey=ea8492c7 `)
+    .then(resp => { return resp.json() })
+    .then(data => getDataUpPoke(data.Search))
+    .catch(error => console.log("Error", error)).catch(error => console.log("Error", error))
+};
+traer(); AzButton.addEventListener("click", () => { traer(title) });
+function traer1(title) {
+  fetch(`https://www.omdbapi.com/?s="${title}"&apikey=ea8492c7`)
+    .then(resp => { return resp.json() })
+    .then(data => getDataDownPoke(data.Search))
+    .catch(error => console.log("Error", error))
+};
+const ZaButton = document.getElementById("ZaButton");
+ZaButton.addEventListener("click",
+  () => { traer1(title) }); 
--- js/movieData.js
+++ js/movieData.js
@@ -1,40 +1,41 @@
 
-function sortArrsToObjects(a, b) { 
-    a = a.Title;
-    b = b.Title;
-  
-    if (a > b) {
-      return 1;
-    } else if (a < b) {
-      return -1;
-    } else if (a === b) {
-      return 0;
-    }
-  }
-  
-  window.sortArrsToObjects = sortArrsToObjects;
-  
-  
-  
-  const getConnection = (busqueda) => {
-    
-    const URL=`http://www.omdbapi.com/?s=${busqueda}&apikey=e41750ae`;
+function sortArrsToObjects(a, b) {
+  a = a.Title;
+  b = b.Title;
 
-    fetch(URL).then(Response=> Response.json())
-    .then(Response=>{let resul=Response.Search
-        resul.forEach(element => {
-            drawMovie(element);
-            
-        });
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  }
+}
+
+window.sortArrsToObjects = sortArrsToObjects;
+
+
+
+const getConnection = (busqueda) => {
+
+  const URL = `http://www.omdbapi.com/?s=${busqueda}&apikey=e41750ae`;
+
+  fetch(URL).then(Response => Response.json())
+    .then(Response => {
+      let resul = Response.Search
+      resul.forEach(element => {
+        drawMovie(element);
+
+      });
     })
 
 
 }
 
 const filterMovie = (data, tipo) => {
-    const arrayT = data.filter(oneMovie => oneMovie.type === tipo || (Array.isArray(oneMovie.type) && oneMovie.type.includes(tipo)));
-    return arrayT;
-  
-  }  
-  window.filterMovie = filterMovie;
-  window.getConnection = getConnection;
+  const arrayT = data.filter(oneMovie => oneMovie.type === tipo || (Array.isArray(oneMovie.type) && oneMovie.type.includes(tipo)));
+  return arrayT;
+
+}
+window.filterMovie = filterMovie;
+window.getConnection = getConnection;
